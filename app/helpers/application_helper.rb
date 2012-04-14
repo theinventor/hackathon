7718baf980e28b164d456d5b42592b8836fcc3d8
Troy Anderson <the.inventor@gmail.com>
2012-04-14 13:53:31 -0700

    commit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
 
-    class Application < Rails::Application
+    #class Application < Rails::Application
 
       CLIENT_ID = "cool_test_app"                 # See https://att.mhealth.com/dev/apps
       CLIENT_SECRET = "Q1neXiRBEGuirVYEmVWBp8W1i5ehFzqfiJdU6AtW"           # See https://att.mhealth.com/dev/apps
@@ -8,6 +8,6 @@ module ApplicationHelper
       MHEALTH_API = "http://api.dev.tfoundry.com/" # mHealth API endpoint (probably won't change)
       REDIRECT_URI = "http://localhost:9292/auth/callback" # Redirect URI to pass to mHealth Connect when user authorizes
 
-    end
+    #end
 
 end
